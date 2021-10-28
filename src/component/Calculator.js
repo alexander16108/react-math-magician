86abe562c9c7148517bec2719e6a00c196c4832d
alexander16108 <alexandermayowa05@gmail.com>
2021-10-28 13:59:24 +0100

    finshed implementation of calculator logic

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,60 +1,64 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import calculate from '../logic/calculate';
-import '../logic/operate'
+import Buttons from './buttons';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      total: null,
-      next: null,
-      operation: null
+      calcObj: {},
     };
+    this.click = this.click.bind(this);
+  }
+
+  click(calcObj, text) {
+    this.setState((calcObj) => calculate(calcObj, text));
   }
 
   render() {
-this.click = (event) => {this.setState((obj)=> {calculate(obj, event.target.textContent)})}
+    let display = '0';
+    const { calcObj } = this.state;
 
-let display;
-
-if(this.state.total || this.state.next || this.state.operation) {
-  display = [this.state.total, ' ', this.state.next, ' ', this.state.operation];
-  display.forEach(element => {element != null });
-} else {
-  display =  0;
-}
+    if (Object.keys(calcObj).length === 0 || (calcObj.total == null
+      && calcObj.next == null && calcObj.operation == null)) {
+      display = '0';
+    } else {
+      display = calcObj.next != null ? calcObj.next
+        : calcObj.next != null ? calcObj.total : calcObj.operation;
+    }
 
     return (
       <div className="calc-container">
-        <div className="calc-firstDisplay">{ display }</div>
+        <div className="calc-firstDisplay">{display}</div>
         <div className="calc-Btns">
           <div className="calc-greyBtns">
             <div className="calc-greyBtn1">
-              <button type="button" onClick={ this.click } className="calc-btn">AC</button>
-              <button type="button" onClick={ this.click } className="calc-btn">+/-</button>
-              <button type="button" onClick={ this.click } className="calc-btn">%</button>
-              <button type="button" onClick={ this.click } className="calc-btn">7</button>
-              <button type="button" onClick={ this.click } className="calc-btn">8</button>
-              <button type="button" onClick={ this.click } className="calc-btn">9</button>
-              <button type="button" onClick={ this.click } className="calc-btn">4</button>
-              <button type="button" onClick={ this.click } className="calc-btn">5</button>
-              <button type="button" onClick={ this.click } className="calc-btn">6</button>
-              <button type="button" onClick={ this.click } className="calc-btn">1</button>
-              <button type="button" onClick={ this.click } className="calc-btn">2</button>
-              <button type="button" onClick={ this.click } className="calc-btn">3</button>
+              <Buttons className="calc-btn" text="AC" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="+/-" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="%" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="7" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="8" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="9" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="4" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="5" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="6" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="1" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="2" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn" text="3" Obj={calcObj} onClick={this.click} />
             </div>
             <div className="calc-greyBtn2">
-              <button type="button" onClick={ this.click } className="calc-btn1">0</button>
-              <button type="button" onClick={ this.click } className="calc-btn2">.</button>
+              <Buttons className="calc-btn1 btn" text="0" Obj={calcObj} onClick={this.click} />
+              <Buttons className="calc-btn2" text="." Obj={calcObj} onClick={this.click} />
             </div>
           </div>
 
           <div className="orange-btn">
-            <button type="button" onClick={ this.click } className="calc-btn">/</button>
-            <button type="button" onClick={ this.click } className="calc-btn">*</button>
-            <button type="button" onClick={ this.click } className="calc-btn">-</button>
-            <button type="button" onClick={ this.click } className="calc-btn">+</button>
-            <button type="button" onClick={ this.click } className="calc-btn">=</button>
+            <Buttons className="calc-btn" text="/" Obj={calcObj} onClick={this.click} />
+            <Buttons className="calc-btn" text="*" Obj={calcObj} onClick={this.click} />
+            <Buttons className="calc-btn" text="-" Obj={calcObj} onClick={this.click} />
+            <Buttons className="calc-btn" text="+" Obj={calcObj} onClick={this.click} />
+            <Buttons className="calc-btn" text="=" Obj={calcObj} onClick={this.click} />
           </div>
         </div>
       </div>
